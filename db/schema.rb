69d9cdddb16a2c80037c784f96d8a093223d7bef
Lia <liasucf@gmail.com>
2018-05-16 16:55:25 -0300

    ajeitando as associacoes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180515193650) do
+ActiveRecord::Schema.define(version: 20180516192345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 20180515193650) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "projects_users", force: :cascade do |t|
+    t.bigint "project_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_projects_users_on_project_id"
+    t.index ["user_id"], name: "index_projects_users_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -87,4 +96,6 @@ ActiveRecord::Schema.define(version: 20180515193650) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects_users", "projects"
+  add_foreign_key "projects_users", "users"
 end
