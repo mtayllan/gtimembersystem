dc28afc7b9687a443e344cfe72451a722cad1f34
mtayllan <messiastayllan@gmail.com>
2018-05-17 20:07:02 +0000

    Terminado tela de Registro de Usuário

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   
   get 'administrative', to: 'administrative/main#index'
   
-  devise_for :admins, controllers: {sessions: 'sign/sessions'}
-  devise_for :users,  controllers: {sessions: 'sign/sessions'}
+  devise_for :admins, :skip => [:registrations], controllers: {sessions: 'sign/sessions'}
+  devise_for :users,  controllers: {sessions: 'sign/sessions', registrations: 'sign/registrations' }
   
   get 'home/index'
   root 'home#index'
